dd062714eb3bd1660e3a5ccd535fdffe3e27eb9b
gnokes <greg.nokes@blueboxgrp.com>
2009-10-30 14:51:35 -0700

    added OSX support

---
+++ lib/OS_10.6_setup.sh
@@ -0,0 +1,6 @@
+#starting setup for OS_10
+
+cd ~ && curl -O http://download.virtualbox.org/virtualbox/3.0.10/VirtualBox-3.0.10-54097-OSX.dmg
+
+echo -e "\n Install VirtualBox, and then install the distro of your choice on it"
+
