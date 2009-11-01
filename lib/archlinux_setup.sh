201b8442e3e17326311d69b0f6971a06d85cbb95
gnokes <greg.nokes@blueboxgrp.com>
2009-11-01 15:05:15 -0800

    archlinux support

--- lib/archlinux_setup.sh
+++ lib/archlinux_setup.sh
@@ -19,10 +19,13 @@ fi
 
 echo -e "\ninstalling base system for building rubies"
 
-sudo pacman -S --noconfirm git subversion gcc gcc-libs kernel-headers zlib libtool bison gdb strace gettext openssl jdk jre
+sudo pacman -S --noconfirm git subversion gcc gcc-libs make kernel-headers zlib libtool bison gdb strace gettext openssl jdk jre 
 
 
 echo -e "\ninstalling database engines"
 
-sudo pacman -S --noconfirm install mysql mysql-clietns postgresql postgresql-libs memcached sqlite-3 
+sudo pacman -S --noconfirm install mysql libmysqlclient mysql-clients postgresq libpqxx postgresql-libs
+l memcached sqlite-3
+
+echo -e "\nmemcached is borked. known issue, and we're working on it..." 
 
